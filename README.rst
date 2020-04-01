698b6f7139f305de025c61758b8b18bf9788f774
Mr. Senko <atodorov@mrsenko.com>
2020-04-01 21:58:32 +0300

    Refactor listener.KiwiTCMS to use tcms_api.plugin_helpers.Backend
    this makes the behavior more in-sync with existing plugins and
    obsoletes the zealand.library module which is very similar.
    
    There is some nice extra integration from the original
    implementation which we keep as much as possible! This is done by
    overriding the Backend class and implementing these extra bits.
    
    Refs #4 b/c I've updated README as well.
    
    Also document how to override and extend with a simple example
    inspired by the original implementation.

--- README.rst
+++ README.rst
@@ -25,6 +25,46 @@ Minimal config file `~/.tcms.conf`::
 
 For more info see `tcms-api docs <https://tcms-api.readthedocs.io>`_.
 
+Each RF suite may define the following variables:
+
+- **${plan_id}**: Existing TestPlan ID for storing test execution results.
+  **WARNING:** this plugin will create a new TestRun whenever ``plan_id``
+  changes! If subsequent test scenarios, aka .robot files don't override
+  ``plan_id`` the current one will be used!
+- **${product}**: Existing product name when creating a new TestPlan
+- **${build_user_email}**: Email for an existing user
+
+.. warning::
+
+    If any of the above variables are missing this plugin will attempt to
+    discover the necessary information from your environment. The exect
+    behavior is document at
+    https://kiwitcms.readthedocs.io/en/latest/plugins/automation-frameworks.html#plugin-configuration
+
+Documentation from test suite(s) is used when creating new TestRun(s).
+
+Each RF test case may also specify the ``TC-xyz`` tag to map to an existing
+TestCase in the database. For example::
+
+    *** Settings ***
+    Documentation   An example test suite
+    Library         OperatingSystem
+
+    *** Variables ***
+    ${plan_id}      234
+
+    *** Test Cases ***
+    Scenario Maps To Existing TestCase
+        [Tags]    TC-607  arbitrary_tag_here
+        Should Be Equal    "Hello"    "Hello"
+
+    Hello World Scenario
+        [Documentation]    This will be the text of the new TC created in DB
+        Should Be Equal    "Hello"    "Hello"
+
+Documentation from test cases is used when creating new TestCase records in
+the database.
+
 
 Usage
 -----
@@ -32,3 +72,66 @@ Usage
 ::
 
     robot --listener zealand.listener.KiwiTCMS
+
+
+Extension and customization
+---------------------------
+
+You can customize the behavior of this plugin by extending the listener and
+backend classes and overriding some of their methods. For example
+save the following in ``acme_tools.py``::
+
+    from zealand.listener import KiwiTCMS
+    from zealand.listener import RFBackend
+
+
+    class JenkinsBackend(RFBackend):
+        def get_run_id(self):
+            """
+                If ${node_name} is specified then tag the TR with "node: X" tag!
+
+                This is done right after a new TR is created!
+            """
+            run_id = super().get_run_id()
+
+            node_name = self.built_in.get_variable_value('${node_name}')
+            if node_name:
+                self.rpc.TestRun.add_tag(run_id, 'node: %s' % node_name)
+            return run_id
+
+        def finish_test_run(self):
+            """
+                Do not set TR.stop_date !!!
+
+                If ${jenkins_tag} is specified then tag the TR with it!
+
+                This is executed at the end of each TestRun after all
+                execution results have been reported.
+            """
+            # do not call the inherited method b/c we want to keep these
+            # test runs open for inspection by a human !!!
+            # super().finish_test_run()
+
+            jenkins_tag = self.built_in.get_variable_value('${jenkins_tag}')
+            if jenkins_tag:
+                self.rpc.TestRun.add_tag(self.run_id, jenkins_tag)
+
+    class AcmeCorpListener(KiwiTCMS):
+        backend_class = JenkinsBackend
+
+        def end_test(self, name, attrs):
+            """
+                You may also find it more appropriate to override a
+                listener class instead.
+            """
+            super().end_test(name, attrs)
+
+            for tag in attrs['tags']:
+                if tag.startswith('JIRA-'):
+                    # hyperlink the results between Kiwi TCMS, Jenkins & JIRA
+                    # by posting comments everywhere
+
+then instruct Robot Framework to use the overriden listener instead of the
+default one::
+
+    robot --listener path/to/acme_tools.py
--- zealand/listener.py
+++ zealand/listener.py
@@ -1,51 +1,216 @@
-from robot.libraries.BuiltIn import BuiltIn
+import os
 
-from zealand.library import ZealandLibrary
+from robot.libraries.BuiltIn import BuiltIn
+from tcms_api.plugin_helpers import Backend
+
+
+class RFBackend(Backend):
+    """
+        Kiwi TCMS plugin backend which has richer integration with
+        Robot Framework internals than standard backend!
+    """
+    built_in = BuiltIn()
+    cwd = os.getcwd() + os.sep
+
+    def default_tester_id(self):
+        """
+            If ${build_user_email} is specified witin the suite then use it,
+            otherwise default to the user connecting via API!
+        """
+        user_email = self.built_in.get_variable_value('${build_user_email}')
+        result = None
+
+        if user_email:
+            result = self.rpc.User.filter({'email': user_email})
+
+        if result:
+            return result[0]['id']
+
+        return super().default_tester_id()
+
+    def external_plan_id(self):
+        """
+            If existing ${plan_id} is specified witin the suite then use it!
+        """
+        plan_id = self.built_in.get_variable_value('${plan_id}')
+        if plan_id:
+            return int(plan_id)
+
+        return super().external_plan_id()
+
+    def get_product_id(self, plan_id):
+        """
+            If existing ${product} is specified witin the suite then use it.
+            Otherwise fall back to discovering the name from the environment!
+        """
+        product_name = self.built_in.get_variable_value('${product}')
+        if product_name:
+            result = self.rpc.Product.filter({'name': product_name})
+            if result:
+                return result[0]['id'], product_name
+
+        return super().get_product_id(plan_id)
+
+    def test_run_update(self, attrs):
+        """
+            Update the currently active TestRun with more information coming
+            out from the Robot Framework test suite.
+        """
+        self.rpc.TestRun.update(self.run_id, {
+            'notes': attrs['doc'],
+        })
+
+    def rf_test_case_get_or_create(self, attrs):
+        """
+            A richer version of Backend.test_case_get_or_create() which
+            allows searching for existing test cases in different ways:
+
+                1. use pre-existing TestCase if specified in the .robot file
+                2. try searching by TC.script && TC.arguments matching the
+                   .robot file and test name
+                3. search by summary if nothing else works
+
+            Then update the TestCase in DB with more information we can
+            obtain from Robot Framework.
+        """
+        created = False
+        test_case = None
+        source_file = attrs['suite']['source'].replace(self.cwd, '')
+
+        # search by explicitly specified pre-existing TestCase
+        for tag in attrs['tags']:
+            tag = tag.lower()
+            if tag.startswith('tc-'):
+                try:
+                    test_case_id = int(tag.replace('tc-', ''))
+
+                    # see if this exists in the DB
+                    result = self.rpc.TestCase.filter({'pk': test_case_id})
+                    if result:
+                        test_case = result[0]
+                    break
+                except ValueError:
+                    pass
+
+        # search by TC.script
+        if test_case is None:
+            result = self.rpc.TestCase.filter({
+                'is_automated': True,
+                'script': source_file,
+                'arguments': attrs['name'],
+                'category__product': self.product_id,
+            })
+            if result:
+                test_case = result[0]
+
+        # search or create by summary if everything else didn't work
+        if test_case is None:
+            test_case, created = super().test_case_get_or_create(
+                attrs['longname'])
+
+        if created:
+            notes = attrs['suite']['doc'] or \
+                'Created by kiwitcms-robotframework-plugin'
+
+            self.rpc.TestCase.update(
+                test_case['id'],
+                {
+                    'text': attrs['doc'],
+                    'notes': notes,
+                    'script': source_file,
+                    'arguments': attrs['name'],
+                },
+            )
+
+        return test_case, created
+
+    def get_status_id(self, name):
+        # translate b/w RF -> Kiwi TCMS statuses
+        # we should make the inherited method add the status if missing
+        # b/c after v8.0 statuses can be customized
+        name = {
+            'PASS': 'PASSED',
+            'FAIL': 'FAILED',
+        }[name]
+        return super().get_status_id(name)
 
 
 class KiwiTCMS:
+    """
+        Listener class for Robot Framework which can be passed to
+        `robot --listener`
+    """
 
-    _shared_state = {}
-    library = None
+    backend_class = RFBackend
     ROBOT_LISTENER_API_VERSION = 2
 
-    def __init__(self):
-        self.__dict__ = self._shared_state
-        self.url = ''
-        self.username = ''
-        self.password = ''
-        self.test_plan = 0
-        self.suite_name = ''
-        self.node = ''
+    # will always trigger at least 1 TR to be created
+    test_plan_id = -1
 
-    def start_suite(self, name, attrs):
-        self.test_plan = BuiltIn().get_variable_value('${plan_Id}')
-        if (self.test_plan is not None and self.suite_name is not name):
-            self.suite_name = name
-            self.url = BuiltIn().get_variable_value('${kiwi_url}')
-            self.username = BuiltIn().get_variable_value('${kiwi_username}')
-            self.password = BuiltIn().get_variable_value('${kiwi_password}')
-            self.library = ZealandLibrary(
-                self.url, self.username, self.password)
-            self.library.set_test_plan(self.test_plan)
-            self.library.set_node(
-                BuiltIn().get_variable_value('${node_name}'))
-            self.library.create_test_run(
-                BuiltIn().get_variable_value('${product}'),
-                BuiltIn().get_variable_value('${build_user_email}'),
-                attrs['doc'])
-        else:
+    def __init__(self):
+        self.suite = None
+        self.backend = self.backend_class(prefix='[RF] ')
+
+    def start_suite(self, name, attrs):  # pylint: disable=unused-argument
+        """
+            If a pre-existing TP was specified then create a TR for it,
+            possibly creating multiple TRs if various suites specify different
+            TPs. Otherwise create a new TP + TR to collect the results.
+        """
+        # skip test suites with empty test list b/c we don't want empty TRs
+        if not attrs['tests']:
             return
 
-    def end_test(self, name, attrs):
-        self.library.check_test_case_and_update(
-            attrs['tags'][0],
-            attrs['status'],
-            name,
-            attrs['doc'],
-            attrs['message'])
+        external_plan_id = self.backend.external_plan_id()
+        # will return 0 if nothing configured ^^^
+
+        # .robot file(s) indicate we need to start a new TR
+        # because there is a TestPlan explicitly specified!
+        if external_plan_id:
+            self.test_plan_id = external_plan_id
+
+        # this is where we create a new TR. All results will be reported
+        # in this TR until some of the suite(s) overrides this
+        if self.test_plan_id != self.backend.plan_id:
+            self.backend.configure()
+            self.test_plan_id = self.backend.plan_id
+
+            self.backend.test_run_update(attrs)
+
+        self.suite = attrs
+
+    def end_test(self, name, attrs):  # pylint: disable=unused-argument
+        attrs.update({
+            'name': name,
+            'suite': self.suite,
+        })
+
+        test_case, _ = self.backend.rf_test_case_get_or_create(attrs)
+        test_case_id = test_case['id']
+
+        self.backend.add_test_case_to_plan(test_case_id, self.backend.plan_id)
+        test_execution_id = self.backend.add_test_case_to_run(
+            test_case_id,
+            self.backend.run_id)
+
+        comment = attrs['message'] or \
+            'Result recorded via kiwitcms-robotframework-plugin'
+        status_id = self.backend.get_status_id(attrs['status'])
+
+        self.backend.update_test_execution(test_execution_id,
+                                           status_id,
+                                           comment)
 
     def end_suite(self, name, attrs):  # pylint: disable=unused-argument
-        if name == self.suite_name:
-            jenkins_tag = BuiltIn().get_variable_value('${jenkins_tag}')
-            self.library.close_test_run(jenkins_tag)
+        """
+            A suite ends so call finish_test_run().
+
+            If there are more suites which report their results within
+            the same TR this will be called multiple times changing
+            TR.stop_date every time.
+
+            If we're actually reporting the results to multiple TRs then
+            each one of them will be finished accordingly.
+        """
+        if self.backend.run_id:
+            self.backend.finish_test_run()
